b5adfc8fb08c9066e0c4398e2a9666e07549c525
李兴 <lixing@jingoal.com>
2016-04-19 16:20:06 +0800

    整理；支持中途插入js语句

--- xtml/xtml.js
+++ xtml/xtml.js
@@ -1,81 +1,49 @@
+/*
+支持if,else, else if
+支持for, for in
+支持中途变量缓存。比如data中有个很深的对象：peoples.people.family,可以使用#var pf = peoples.people.family把它存下来，在后面用pf访问此数据
+*/
 var str = '\
 <div id="{{$a}}">\
-<span>{{~$arr as value,index}} {{? (index > 2)  }}<em>{{value}}</em> {{/?}} {{/~}} </span> <br/>\
+<span>{{~$arr as value,index}} {{? index > 2  }}<em>{{value}}</em> {{/?}} {{/~}} </span> <br/>\
 <span>{{~$obj in value,index}} <em>{{value}}</em> {{/~}} </span>\
+</div>\
+<div id="peoples">\
+    {{~$peoples as people,index}}\
+    <p>\
+        {{#var pf = people.family}}\
+        name:{{index}},{{people.name}}, family:{{pf.join(\',\')}}\
+    </p>\
+    {{/~}}\
 </div>\
 \
 ';
 var trim = function (str) {
     return str.replace(/^\s+/,'').replace(/\s+$/, '');
 }
-function arrFor (obj, value, key) {
-    return 'for (var $3 = 0, len = $1.length, $2; $3<len;$3++) { \n$2 = $1[$3];'.replace('$1', obj).replace('$2', value).replace('$3', key);
+var 
+varReg = /\$([\w\$]+)/g,
+blockReg = /\{\{([\s\S]+?)\}\}/g,
+blockEndReg = /\{\{\s*\/[\~\?]\s*\}\}/g,
+forloopReg = /\~(\S+)\s+(?:as|in)\s+([\w\$]+)?,?([\w\$]+)?/,
+blankStrReg = /out\+\='\s?';?\n?/g;
+var objId = 0;
+var startEnd = {
+    newline:{
+        start:'\';\n', end:'\nout+=\''
+    },
+    joinline:{
+        start:"\'+", end:"+\'"
+    }
+},
+newstart = startEnd.newline.start,
+newend = startEnd.newline.end,
+joinstart = startEnd.joinline.start,
+joinend = startEnd.joinline.end;
+function varReplace ($1) {
+    return $1.replace(varReg, 'it.$1')
 }
-function objFor (obj, value, key) {
-    return 'for (var $3 in $1) { \n$2 = $1[$3];'.replace('$1', obj).replace('$2', value).replace('$3', key);
-}
-var compiled = 'var out = \'' 
-        + str
-        .replace(/\{\{\/[\~\?]\}\}/g, '\'\n}\nout+=\'')
-        .replace(/\{\{(\?{1,2})([\s\S]+?)\}\}/g, function (m, isif, condition, c) {
-            condition = trim(condition);
-            if(isif ==='?') {
-                return '\'\nif(' + condition + '){\nout+=\''
-            }
-            return condition ? '\nelse if(' + condition + '){\nout+=\'' : 'else {\n';
-            console.log(m, '---------',a, '---------',b, '---------',c)
-        })
-        /*.replace(/\~(\S+)\s+in\s+([\w\$]+)?,?([\w\$]+)?/, function (m, obj, value, key) {
-            if (m.indexOf(' in ') > -1) {
-                    m = objFor(obj, value, key);
-                } else {
-                    m = arrFor(obj, value, key);
-                }
-                return "\';\n"+ m.replace(/\$([\w\$]+)/g, 'it.$1') + "\nout+=\'";
-        })*/
-        .replace(/\{\{([\s\S]+?)\}\}/g, function (m, $1, $2) {
-            console.log(m,'---------', $1,'---------', $2);
-            $1 = trim($1);
-            if($1.charAt(0) === '~') {
-                var arr = $1.split(/\s+/);
-                console.log(/\~(\S+)\s+as\s+([\w\$]+)?,?([\w\$]+)?/.test($1))
-                if ($1.indexOf(' in ') > -1) {
-                    console.log($1)
-                    $1 = $1.replace(/\~(\S+)\s+in\s+([\w\$]+)?,?([\w\$]+)?/, 'for (var $3 in $1) { \n$2 = $1[$3];');
-                    /*$1 = $1.replace(/\~(\S+)\s+in\s+([\w\$]+)?,?([\w\$]+)?/, function (m, obj, value, key) {
-                        console.log(m, obj, value, key);
-                        return 'for(var ' + key + ' in ' + obj + ') { ' + value + ' = ' + obj + '[' + key + '];';
-                    });*/
-                } else {
-                    $1 = $1.replace(/\~(\S+)\s+as\s+([\w\$]+)?,?([\w\$]+)?/, 'for (var $3 = 0, len = $1.length, $2; $3<len;$3++) { \n$2 = $1[$3];');
-                    /*$1 = $1.replace(/\~(\S+)\s+as\s+([\w\$]+)?,?([\w\$]+)?/, function (m, obj, value, key) {
-                        return 'for (var '+ key +' = 0, '+ value +', len = '+obj+'.length; '+key+'<len;'+key+'++) {'+value+' = '+obj+'['+key+']; ';
-                    });*/
-                }
-                return "\';\n"+ $1.replace(/\$([\w\$]+)/g, 'it.$1') + "\nout+=\'";
-            }
-            return "\' + " + $1.replace(/\$([\w\$]+)/g, 'it.$1') + "+ \'";
-        })
-        + '\';return out'
-    
-    // .replace(/\{\{\~(\S+)\s+as|in\s+([\w\$]+)?,?([\w\$]+)?\}\}/, function (m, $1, $2, $3) {
-       
-    // })
-var srcdata = {
-        a:'这是a',
-        obj:{
-            aaa:'aaa in obj',
-            bbb:'bbb in obj'
-        },
-        arr:[11,22,33,44,77]
-    };
-var compileFn = new Function('it', compiled);
-console.log(
-    compileFn, compileFn(srcdata)
-)
-$('mb').innerHTML = str;
-$('by').innerHTML = compiled;
-$('result').innerHTML = compileFn(srcdata);
+
 var xtml = function (str, data) {
 	return 
 }
@@ -83,21 +51,53 @@ xtml.codeBlock = {
 	start:'{{',
 	end:'}}'
 }
-var varReg = new RegExp('\\s('+ xtml.varSymbol +')[\\S]+');
 xtml.varSymbol = '$';
 function compile(str) {
-	var fnStr = 'var result = \'\';';
-	var lastIndex = 0;
-	var reg = new RegExp(xtml.codeBlock.start+'([^}]+)'+xtml.codeBlock.end, 'g');
-	var repStr = str.replace(reg, function (m, $1) {
-		return m.replace(varReg, function (mm, $$1) {
-			return 'it.';
-		})
-	})
-	// 对象遍历
-	.replace(/\{\{\~\s*(\S+)(as)?\s+(\S+),?(\S+)?/, function (m) {
-		return
-	})
+	return new Function('it', 'var out=\'\';' + newend 
+        + str
+        .replace(blockEndReg, newstart+'}'+newend)
+        .replace(/\{\{(\?{1,2})([\s\S]+?)\}\}/g, function (m, isif, condition, c) {
+            condition = trim(condition);
+            if(isif === '?') {
+                return newstart+'if(' + condition + '){'+newend
+            }
+            return (condition ? '\nelse if(' + condition + '){' : '\nelse {')+newend;
+        })
+        // .replace(/\~(\S+)\s+(in|as)\s+([\w\$]+)?,?([\w\$]+)?/, function (ma, obj, type, value, key) {
+        //     console.log(ma, obj, value, key)
+        //     if (type === 'in') {
+        //         ma = 'var ' + key + ',' + value + ';\nfor(' + key + ' in ' + obj + ') { ' + value + ' = ' + obj + '[' + key + '];';
+        //     } else {
+        //         ma = 'for (var arr_'+(objId+=1)+'=' + obj + ','+ key +' = 0, '+ value +', arr_len_'+objId+' = '+ obj +'.length; '+key+'<arr_len_'+objId+';'+key+'++) {'+value+' = arr_'+objId+'['+key+']; ';
+        //     }
+        //     console.log(varReplace(ma), newstart)
+        //     return newstart+ varReplace(ma) + newend;
+        // })
+        .replace(blockReg, function (m, $1, first) {
+            // console.log(m,'---------', $1,'---------', $2);
+            $1 = trim($1);
+            first = $1.charAt(0);
+            if(first === '~') {
+                // console.log(forloopReg.test($1))
+                $1 = $1.replace(
+                        forloopReg,
+                        $1.indexOf(' in ') > -1 
+                        ? 'var $2,$3;\nfor ($3 in $1) { \n$2 = $1[$3];'
+                        : 'for (var arr_'+(objId+=1)+'=$1, $3 = 0, arr_len_'+objId+' = $1.length, $2; $3<arr_len_'+objId+';$3++) { \n$2 = arr_'+objId+'[$3];'
+                    );
+                return newstart + varReplace($1) + newend;
+            }
+            else if(first === '#') {                
+                return newstart + varReplace($1.slice(1)) + newend;
+            }
+            console.log($1,varReplace($1))
+            return joinstart + varReplace($1) + joinend;
+        })
+        // 去除空的 out += ''这样的字符串
+        .replace(blankStrReg,'')
+        + newstart
+        + 'return out;'
+    )
 }
 function aa(it) {
 	var result = '';
@@ -110,3 +110,10 @@ function aa(it) {
 	result += '</span>';
 	result += '</div>';
 }
+
+var compileFn = compile(str);
+console.log(
+    compileFn, compileFn(srcdata)
+)
+$('mb').innerHTML = str;
+$('result').innerHTML = compileFn(srcdata);
