29f16fcb9ea2f4ef9af203ba2ceb27109ec62df3
李兴 <lixing@jingoal.com>
2016-04-26 18:33:16 +0800

    规范化一下代码

--- xtml/xtml.js
+++ xtml/xtml.js
@@ -3,27 +3,28 @@
 支持for, for in
 支持中途变量缓存。比如data中有个很深的对象：peoples.people.family,可以使用#var pf = peoples.people.family把它存下来，在后面用pf访问此数据
 */
-var xtml = (function () {    
+var xtml = (function () {
+    'use strict';
     var trim = function (str) {
-        return str.replace(/^\s+/,'').replace(/\s+$/, '');
-    },     
-    varReg = /\$([\w\$]+)/g,
-    ifelseReg = /\{\{(\?{1,2})([\s\S]+?)\}\}/g,
-    blockReg = /\{\{([\s\S]+?)\}\}/g,
-    blockEndReg = /\{\{\s*\/[\~\?]\s*\}\}/g,
-    brReg =/(\n|\r|\t)/g,
-    forloopReg = /(\S+)\s+(?:as|in)\s+([\w\$]+)?,?([\w\$]+)?/,
-    blankStrReg = /out\+\='\s?';?\n?/g;
-    objId = 0,
-    newlinestart = "';\n",
-    newlineend = "\nout+='",
-    joinlinestart = "'+",
-    joinlineend = "+'";
-    function varReplace ($1) {
-        return $1.replace(varReg, 'it.$1')
+            return str.replace(/^\s+/, '').replace(/\s+$/, '');
+        },
+        varReg = /\$([\w\$]+)/g,
+        ifelseReg = /\{\{(\?{1,2})([\s\S]+?)\}\}/g,
+        blockReg = /\{\{([\s\S]+?)\}\}/g,
+        blockEndReg = /\{\{\s*\/[\~\?]\s*\}\}/g,
+        brReg = /(\n|\r|\t)/g,
+        forloopReg = /(\S+)\s+(?:as|in)\s+([\w\$]+)?,?([\w\$]+)?/,
+        blankStrReg = /out\+\='\s?';?\n?/g,
+        objId = 0,
+        newlinestart = "';\n",
+        newlineend = "\nout+='",
+        joinlinestart = "'+",
+        joinlineend = "+'";
+    function varReplace($1) {
+        return $1.replace(varReg, 'it.$1');
     }
     var templateReplace = {
-        '~':function forloopReplace ($1) {
+        '~': function forloopReplace($1) {
             $1 = varReplace($1.replace(
                     forloopReg,
                     $1.indexOf(' in ') > -1 
@@ -32,22 +33,21 @@ var xtml = (function () {
                 ));
             return newlinestart + $1 + newlineend;
         },
-        '#':function ($1) {    
+        '#': function ($1) {
             return newlinestart + varReplace($1) + newlineend;
         }
-    }
+    };
     function compile(str) {
-    	return new Function('it', "var out='';" + newlineend 
-            // 
+        return new Function('it', "var out='';" + newlineend
             + trim(str)
-            .replace(brReg,'\\$1')
-            .replace(blockEndReg, newlinestart+'}'+newlineend)
-            .replace(ifelseReg, function (m, isif, condition, c) {
+            .replace(brReg, '\\$1')
+            .replace(blockEndReg, newlinestart + '}' + newlineend)
+            .replace(ifelseReg, function (m, isif, condition) {
                 condition = trim(condition);
-                if(isif === '?') {
-                    return newlinestart+'if(' + condition + '){'+newlineend
+                if (isif === '?') {
+                    return newlinestart + 'if(' + condition + '){' + newlineend;
                 }
-                return (condition ? '\nelse if(' + condition + '){' : '\nelse {')+newlineend;
+                return (condition ? '\nelse if(' + condition + '){' : '\nelse {') + newlineend;
             })
             .replace(blockReg, function (m, $1, replaceFn) {
                 // console.log(m,'---------', $1,'---------', $2);
@@ -55,10 +55,10 @@ var xtml = (function () {
                 return (replaceFn = templateReplace[$1.charAt(0)]) ? replaceFn($1.substr(1)) : joinlinestart + varReplace($1) + joinlineend;
             })
             // 去除空的 out += ''这样的字符串
-            .replace(blankStrReg,'')
+            .replace(blankStrReg, '')
             + newlinestart
             + 'return out;'
-        )
+        );
     }
     return compile;
-})();
+}());
